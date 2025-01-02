b2e895c3af4199c1db654f6329a7d9af8be29312
trikmgithub <trikmforwork@gmail.com>
2025-01-02 20:40:00 +0700

    Logic search header

--- src/components/AccountItem/index.jsx
+++ src/components/AccountItem/index.jsx
@@ -2,27 +2,26 @@ import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
 import icons from '~/utils/icons';
 import Image from '../Image';
+import { Link } from 'react-router';
 
 const { PiSealCheckFill } = icons;
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
     return (
         <>
-            <div className={cx('wrapper')}>
+            <Link to={`/@${encodeURIComponent(data.nickname)}`} className={cx('wrapper')}>
                 <span className={cx('avatar')}>
-                    <Image className={cx('avatar')} src="vite.svgg" alt="Username" />
+                    <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
                 </span>
                 <div className={cx('info')}>
                     <h4 className={cx('name')}>
-                        <span>Kieu Minh Tri</span>
-                        <span className={cx('ri-check')}>
-                            <PiSealCheckFill />
-                        </span>
+                        <span>{data.full_name}</span>
+                        <span className={cx('ri-check')}>{data.tick && <PiSealCheckFill />}</span>
                     </h4>
-                    <span className={cx('username')}>trikm</span>
+                    <span className={cx('username')}>{data.nickname}</span>
                 </div>
-            </div>
+            </Link>
         </>
     );
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -17,14 +17,28 @@ function Search() {
     const [searchValue, setSearchValue] = useState('');
     const [searchResult, setSearchResult] = useState([]);
     const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
 
     const inputRef = useRef();
 
     useEffect(() => {
-        setTimeout(() => {
-            setSearchResult([1, 2, 3]);
-        }, 0);
-    }, []);
+        if (!searchValue.trim()) {
+            setSearchResult([]);
+            return;
+        }
+
+        setLoading(true);
+
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+            .then((res) => res.json())
+            .then((res) => {
+                setSearchResult(res.data);
+                setLoading(false);
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    }, [searchValue]);
 
     const handleClear = () => {
         setSearchValue('');
@@ -44,9 +58,9 @@ function Search() {
                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                     <PopperWrapper>
                         <h4 className={cx('search-title')}>Accounts</h4>
-                        <AccountItem />
-                        <AccountItem />
-                        <AccountItem />
+                        {searchResult.map((result) => (
+                            <AccountItem key={result.id} data={result} />
+                        ))}
                     </PopperWrapper>
                 </div>
             )}
@@ -62,15 +76,17 @@ function Search() {
                     onFocus={() => setShowResult(true)}
                 />
 
-                {!!searchValue && (
+                {!!searchValue && !loading && (
                     <button className={cx('ri-close')} onClick={handleClear}>
                         <IoMdCloseCircle />
                     </button>
                 )}
 
-                {/* <span className={cx('ri-loader')}>
-                    <BiLoaderCircle />
-                </span> */}
+                {loading && (
+                    <span className={cx('ri-loader')}>
+                        <BiLoaderCircle />
+                    </span>
+                )}
 
                 <button className={cx('search-btn')}>
                     <SearchIcon />
